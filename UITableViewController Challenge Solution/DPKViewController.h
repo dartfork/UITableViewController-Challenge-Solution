ad0db9b55094fea2f29c4bcc47d22c084aa1246e
Tania Dantas <taniadantas@Tania-Dantass-MacBook-Pro.local>
2014-04-21 23:04:27 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/DPKViewController.h
@@ -0,0 +1,13 @@
+//
+//  DPKViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Tania Dantas on 4/21/14.
+//  Copyright (c) 2014 dartfork sound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPKViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Tania Dantas on 4/21/14.
+//  Copyright (c) 2014 dartfork sound. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPKAppDelegate class]));
+    }
+}
